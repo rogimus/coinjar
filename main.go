ccefc99a2a551885dd3bf2116f12b25386ad789f
Roger Murray <roger.h.murray@gmail.com>
2022-03-10 14:45:46 -0500

    Move packages to ./internal directory

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"time"
 	
-	"github.com/rogimus/coinjar/orderbook"
+	"github.com/rogimus/coinjar/internal/orderbook"
 )
 
 func main() {
 
 	for {
-		orderbook.AddToData("BTCAUD", "2")
-		orderbook.AddToData("ETHAUD", "2")
+		go orderbook.AddToData("BTCAUD", "2")
+		go orderbook.AddToData("ETHAUD", "2")
 		time.Sleep(5 * time.Minute)
 	}
 }
